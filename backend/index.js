698fe8a0ce576d16cf6477e6e114de95623bdd77
AGoutieras <goutieras.anthony@gmail.com>
2026-05-28 12:24:57 +0200

    feat: add JWT middleware

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
-dotenv.config()
-
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import './src/db/index.js'
 import authRouter from './src/routes/auth.js'
+import authMiddleware from './src/middleware/auth.js'
 
+dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT
@@ -16,6 +16,9 @@ app.use(express.json())
 
 app.use('/auth', authRouter)
 
-app.listen(PORT, () => {
+app.use(authMiddleware)
+
+app.listen(PORT, () =>
+{
   console.log("Server listening on PORT", PORT)
 })
